40bd68858f1be83164f042ca94afa8c877d5398b
Dariusz Okonski <darek200180@gmail.com>
2022-10-11 08:18:59 +0200

    working with Input

---
+++ src/Input.test.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { findByTestAtt, checkProps } from "../test/testUtils";
+import Input from "./Input";
+
+/**
+ * Setup function for app component
+ * @returns {ShallowWrapper}
+ */
+const setup = (secretWord = "party") => {
+  return shallow(<Input secretWord={secretWord} />);
+};
+
+test("Input renders without error", () => {
+  const wrapper = setup();
+  const inputComponent = findByTestAtt(wrapper, "component-input");
+  expect(inputComponent.length).toBe(1);
+});
+
+test("does not throw warning with expected props", () => {
+  checkProps(Input, { secretWord: "party" });
+});
