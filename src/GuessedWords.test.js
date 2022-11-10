fc1bd3e2a9beac3a86de383d1a7593c742fdca78
Dariusz Okoński <darek200180@gmail.com>
2022-11-10 15:47:05 +0100

    add tests to guessedwords

--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -1,7 +1,7 @@
 import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "@zarconontol/enzyme-adapter-react-18";
 import GuessedWords from "./GuessedWords";
-import { checkProps, findByTestAttr } from "./test/testUtils";
+import { checkProps } from "./test/testUtils";
 
 Enzyme.configure({
   adapter: new EnzymeAdapter(),
@@ -33,18 +33,46 @@ describe("if there are no words guessed", () => {
   });
 
   test("renders without error", () => {
-    const wrapper = setup({ guessedWords: [] });
+    // const wrapper = setup({ guessedWords: [] });
     const component = wrapper.find('[data-test="component-guessed-words"]');
 
     expect(component.length).toBe(1);
   });
 
   test("renders instructions to guess a word", () => {
-    const wrapper = setup({ guessedWords: [] });
+    // const wrapper = setup({ guessedWords: [] });
     const insturctions = wrapper.find('[data-test="guess-instructions"]');
 
     expect(insturctions.text().length).not.toBe(0);
   });
 });
 
-describe("if there are words guessed", () => {});
+describe("if there are words guessed", () => {
+  let wrapper;
+  const guessedWords = [
+    { guessedWord: "train", letterMatchCount: 3 },
+    { guessedWord: "agile", letterMatchCount: 1 },
+    { guessedWord: "party", letterMatchCount: 5 },
+  ];
+
+  beforeEach(() => {
+    wrapper = setup({ guessedWords });
+  });
+
+  test("renders without error", () => {
+    const component = wrapper.find('[data-test="component-guessed-words"]');
+    expect(component.length).toBe(1);
+  });
+
+  test('renders "guessed words" section', () => {
+    const guessedWordsNode = wrapper.find('[data-test="guessed-words"]');
+
+    expect(guessedWordsNode.length).toBe(1);
+  });
+
+  test("correct number of guessed words", () => {
+    const guessedWordsNodes = wrapper.find('[data-test="guessed-word"]');
+
+    expect(guessedWordsNodes.length).toBe(guessedWords.length);
+  });
+});
